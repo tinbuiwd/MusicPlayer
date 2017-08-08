cd75f1927c69f48c216980c8740fbc7bd7cbccc1
Bùi Văn Tín <tinbui.wd@gmail.com>
2017-08-08 13:29:32 +0700

    Add files via upload
    first version

---
+++ demoMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoMusic
+//
+//  Created by Bui Van Tin on 7/19/17.
+//  Copyright © 2017 Bui Van Tin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demoMusic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demoMusic
+//
+//  Created by Bui Van Tin on 7/19/17.
+//  Copyright © 2017 Bui Van Tin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
